b96450e3b836f4d2f549cf334fcb26e6e694f46d
SemKaminskyi <kaminskyisem@gmail.com>
2020-06-16 00:43:48 +0300

    add correct main activity2 but have some problem with size of button and cardView

--- app/src/main/java/com/gmail/kaminskysem/PersnalHelper/MainActivity2.java
+++ app/src/main/java/com/gmail/kaminskysem/PersnalHelper/MainActivity2.java
@@ -3,12 +3,50 @@ package com.gmail.kaminskysem.PersnalHelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 public class MainActivity2 extends AppCompatActivity {
+    private static String LOG_TAG = MainActivity2.class.getSimpleName();
 
+
+    // TODO: 16.06.2020 cheaking intent file and replace code of intent in MAnifest
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Log.d(LOG_TAG, "OnCreate, "+this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart, "+this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume, "+this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause, "+this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop, "+this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy, "+this);
     }
 }
