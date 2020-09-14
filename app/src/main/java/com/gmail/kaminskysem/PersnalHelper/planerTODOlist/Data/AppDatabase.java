aa58e252f4b35ab8334793402d1bca178f08d1f8
SemKaminskyi <kaminskyisem@gmail.com>
2020-09-14 09:36:58 +0300

    add ROOM DB

---
+++ app/src/main/java/com/gmail/kaminskysem/PersnalHelper/planerTODOlist/Data/AppDatabase.java
@@ -0,0 +1,13 @@
+package com.gmail.kaminskysem.PersnalHelper.Data;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.gmail.kaminskysem.PersnalHelper.planerTODOlist.IUserPlanerDao;
+import com.gmail.kaminskysem.PersnalHelper.planerTODOlist.forRecyclerView.model.PlanerDetails;
+
+@Database(entities = {PlanerDetails.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract IUserPlanerDao userPlanerDao();
+}
